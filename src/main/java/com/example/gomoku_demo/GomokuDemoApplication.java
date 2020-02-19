b515149d10ffa198015b7e123a906250fe70d98f
Quang Huy Le <beatboy2603@gmail.com>
2020-02-19 14:17:14 +0700

    Merge origin/feature/improveDoExam
    Conflicts:
    	src/main/java/com/hgedu_server/controllers/ClassController.java
    	src/main/java/com/hgedu_server/models/Question.java
    	src/main/java/com/hgedu_server/repositories/ClassRepository.java
    	src/main/java/com/hgedu_server/services/ClassService.java

---
+++ src/main/java/com/example/gomoku_demo/GomokuDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gomoku_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomokuDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GomokuDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/gomoku_demo/controller/TestController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.gomoku_demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author ADMIN
+ */
+@Controller
+public class TestController {
+    @GetMapping("/")
+    public String a(){
+        System.out.println("abcd");
+        return "index";
+    }
+}
